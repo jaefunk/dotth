3db1b7149c65b35becd6f0a54d71306fb5b89d40
jaefunk <jaefunk@me.com>
2019-12-05 18:33:46 +0900

    work on blendfunc

--- build/microsoft/testdotth/testscene.cpp
+++ build/microsoft/testdotth/testscene.cpp
@@ -6,14 +6,13 @@ void testscene::init(const json& data)
 	auto parent = std::make_shared<dotth::rectangle>();
 	parent->init();
 	parent->set_color({ 1.f, 0.f, 0.f, 0.5f });
-	parent->trans.scl(0.5f);
 	parent->trans.rot_z(45.f);
 	attach(parent);
 
 	auto child = std::make_shared<dotth::rectangle>();
 	child->init();
-	child->set_color({ 1.f, 1.f, 0.f, 1.f });
-	child->trans.pos_x(2.f);
+	child->set_color({ 0.f, 1.f, 0.f, 1.f });
+	child->trans.pos_x(0.5f);
 	parent->attach(child);
 }
 
