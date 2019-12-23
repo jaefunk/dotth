aac459a0500c228a599e076753301013b8a6166c
jaefunk <jaefunk@me.com>
2019-12-23 13:59:51 +0900

    work on framebuffer

--- core/renderer/renderer.cpp
+++ core/renderer/renderer.cpp
@@ -14,6 +14,18 @@ void dotth::gl_callback::display(void) {
 	renderer::instance()->process(dotth::render::draw_type::perspective);
 	renderer::instance()->process(dotth::render::draw_type::orthographic);
 
+
+	//unsigned int fbo;
+	//glGenFramebuffers(1, &fbo);
+	//glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	//glClearColor(0.1f, 0.1f, 0.1f, 1.f);
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glEnable(GL_DEPTH_TEST);
+	//glDepthFunc(GL_LESS);
+	//renderer::instance()->process(dotth::render::draw_type::perspective);
+	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
 	renderer::instance()->flush();
 
 	glutSwapBuffers();
@@ -38,8 +50,8 @@ void dotth::renderer::init_gl(int argc, char** argv) {
     glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
     glutInitWindowSize(1024, 512);
     glutInitWindowPosition(0, 0);
-    glClearColor(0, 0, 0, 0);
 	glutCreateWindow("asdf");
+	glClearColor(0.25f, 0.25f, 0.25f, 1.f);
 	glEnable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
@@ -73,6 +85,26 @@ void dotth::renderer::push_back(dotth::render::command::inherit * inherit)
 {
 	_drawing_object_cnt++;
 	_queue[inherit->_type].push_back(inherit);
+
+	//unsigned int fbo;
+	//glGenFramebuffers(1, &fbo);
+	//glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	//glClearColor(0.1f, 0.1f, 0.1f, 1.f);
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glEnable(GL_DEPTH_TEST);
+	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	//if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE)
+	//{
+	//	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, 999, 0);
+	//}
+	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	//glDeleteFramebuffers(1, &fbo);
+
+	//unsigned int rbo;
+	//glGenRenderbuffers(1, &rbo);
+	//glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	//glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 1024, 512);
+	//glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
 }
 
 void dotth::renderer::process(const dotth::render::draw_type & type) {
